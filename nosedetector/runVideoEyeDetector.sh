336532429ab687133d5b29603bad1bd0b6a03d2f
Eric Yan <digimonyan@gmail.com>
2021-04-08 19:58:51 -0400

    made a nose+face+eyes detector for video frames

---
+++ nosedetector/runVideoEyeDetector.sh
@@ -0,0 +1,1 @@
+python videoEyeDetector.py --face_cascade ./frontalFace10/haarcascade_frontalface_default.xml --eyes_cascade haarcascade_eye_tree_eyeglasses.xml
---
+++ nosedetector/runVideoNostDetector.sh
@@ -0,0 +1,1 @@
+python videoNoseDetector.py --face_cascade ./frontalFace10/haarcascade_frontalface_alt_tree.xml --nose_cascade nose25x15.xml
