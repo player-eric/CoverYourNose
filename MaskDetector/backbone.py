efdc9e387902412ed58b7a751261402333462ba5
Sam Wilkins <samwilkins333@gmail.com>
2021-04-09 13:20:32 -0700

    initial backbone script, pre-test on GPU enabled machine

---
+++ MaskDetector/backbone.py
@@ -0,0 +1,55 @@
+import torch
+from matplotlib import pyplot as plt
+import matplotlib.patches as patches
+
+# import ssl
+# ssl._create_default_https_context = ssl._create_unverified_context
+
+# https://pytorch.org/hub/nvidia_deeplearningexamples_ssd/
+
+precision = "fp32"
+threshold = 0.40
+
+ssd_model = torch.hub.load(
+    'NVIDIA/DeepLearningExamples:torchhub',
+    'nvidia_ssd',
+    model_math=precision,
+    map_location=torch.device('cpu')
+)
+ssd_model.eval()
+
+utils = torch.hub.load('NVIDIA/DeepLearningExamples:torchhub', 'nvidia_ssd_processing_utils')
+
+uris = [
+    'http://images.cocodataset.org/val2017/000000397133.jpg',
+    'http://images.cocodataset.org/val2017/000000037777.jpg',
+    'http://images.cocodataset.org/val2017/000000252219.jpg'
+]
+
+inputs = [utils.prepare_input(uri) for uri in uris]
+tensor = utils.prepare_tensor(inputs, precision == "fp16")
+
+with torch.no_grad():
+    detections_batch = ssd_model(tensor)
+
+results_per_input = utils.decode_results(detections_batch)
+best_results_per_input = [utils.pick_best(results, threshold) for results in results_per_input]
+
+classes_to_labels = utils.get_coco_object_dictionary()
+
+for image_idx in range(len(best_results_per_input)):
+    fig, ax = plt.subplots(1)
+    # Show original, denormalized image...
+    image = inputs[image_idx] / 2 + 0.5
+    ax.imshow(image)
+    # ...with detections
+    bboxes, classes, confidences = best_results_per_input[image_idx]
+    for idx in range(len(bboxes)):
+        left, bot, right, top = bboxes[idx]
+        x, y, w, h = [val * 300 for val in [left, bot, right - left, top - bot]]
+        rect = patches.Rectangle((x, y), w, h, linewidth=1, edgecolor='r', facecolor='none')
+        ax.add_patch(rect)
+        ax.text(x, y, "{} {:.0f}%".format(classes_to_labels[classes[idx] - 1], confidences[idx]*100),
+                bbox=dict(facecolor='white', alpha=0.5))
+
+plt.savefig("/.output.png")
